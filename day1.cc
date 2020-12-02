94a240df2d9d2e94fcb2bf6ed716308db3fd24b3
Filip Niksic <fniksic@seas.upenn.edu>
2020-12-01 23:20:01 -0500

    Solve the Day 1 puzzle

--- day1.cc
+++ day1.cc
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <unordered_set>
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  std::unordered_set<int> entries;
+  while (!std::cin.eof()) {
+    int n;
+    std::cin >> n;
+    if (entries.find(2020 - n) != entries.end()) {
+      std::cout << (2020 - n) * n << std::endl;
+      break;
+    }
+    entries.insert(n);
+  }
   return 0;
 }
